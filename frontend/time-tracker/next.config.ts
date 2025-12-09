dee5d9ba31cbf261e20d0a5367645ee25abeaf0f
Totenem <yganamichaelashley@gmail.com>
2025-12-09 09:15:35 +0800

    fix: fixing url issues still

--- frontend/time-tracker/next.config.ts
+++ frontend/time-tracker/next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-
-  // Expose env vars to the client
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
 };
-
-console.log("NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL);
 
 export default nextConfig;
