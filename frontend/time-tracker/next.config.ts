9d215b052b78d86b9a3379edc132c404b8b58aea
Totenem <yganamichaelashley@gmail.com>
2025-12-09 08:57:37 +0800

    fix: fixing url issue env file not being read in prod

--- frontend/time-tracker/next.config.ts
+++ frontend/time-tracker/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: "standalone",
+
+  // Expose env vars to the client
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
+
+console.log("NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL);
 
 export default nextConfig;
